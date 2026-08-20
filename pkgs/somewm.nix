924ca13dab7e4fedc7a7a80b81311be750bd0d49
ThunderSync <gianfrancoferretti98@gmail.com>
2026-08-20 13:06:58 -0300

    fix: avoid conflicting LGI patch bootstrap

--- pkgs/somewm.nix
+++ pkgs/somewm.nix
@@ -2,9 +2,7 @@
 
 let
   lua = pkgs.lua5_3;
-  lgi = pkgs.lua53Packages.lgi.overrideAttrs (old: {
-    patches = (old.patches or []) ++ [ ./patches/lgi-glib-2.86.patch ];
-  });
+  lgi = pkgs.lua53Packages.lgi;
   wlroots = pkgs."wlroots_${builtins.replaceStrings [ "." ] [ "_" ] wlrootsVersion}";
   giTypelibPath = pkgs.lib.makeSearchPath "lib/girepository-1.0" [
     pkgs.glib
@@ -57,7 +55,6 @@ pkgs.stdenv.mkDerivation {
     export PKG_CONFIG_SYSTEMD_SYSTEMDSYSTEMUNITDIR="$out/lib/systemd/system"
   '';
 
-  # Build-time paths are needed for Meson/configuration and the package tests.
   LUA_PATH = "${lgi}/share/lua/5.3/?.lua;${lgi}/share/lua/5.3/?/init.lua;;";
   LUA_CPATH = "${lgi}/lib/lua/5.3/?.so;${lgi}/lib/lua/5.3/lgi/?.so;;";
 
@@ -66,16 +63,6 @@ pkgs.stdenv.mkDerivation {
       --set LUA_PATH "${lgi}/share/lua/5.3/?.lua;${lgi}/share/lua/5.3/?/init.lua;;" \
       --set LUA_CPATH "${lgi}/lib/lua/5.3/?.so;${lgi}/lib/lua/5.3/lgi/?.so;;" \
       --set GI_TYPELIB_PATH "${giTypelibPath}"
-
-    env \
-      LUA_PATH="${lgi}/share/lua/5.3/?.lua;${lgi}/share/lua/5.3/?/init.lua;;" \
-      LUA_CPATH="${lgi}/lib/lua/5.3/?.so;${lgi}/lib/lua/5.3/lgi/?.so;;" \
-      GI_TYPELIB_PATH="${giTypelibPath}" \
-      ${lua}/bin/lua -e '
-        local lgi = require("lgi")
-        assert(lgi.GLib, "LGI loaded without GLib")
-        assert(lgi.GObject, "LGI loaded without GObject")
-      '
   '';
 
   mesonBuildType = "release";
